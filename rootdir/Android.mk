44fca3226e3063942f2e07e22f7af87535590920
mcrazymanu <mcrazymanu@gmail.com>
2022-09-10 00:51:05 +0530

    added a80

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -146,7 +146,7 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := wifi_a70q.rc
+LOCAL_MODULE       := wifi_a80.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/wifi.rc
